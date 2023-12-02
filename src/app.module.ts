fa3c823b4cad05a7a861d3ed2aa80320dcaf5776
eebhenga ebhenga <jean_quentin2001@yahoo.fr>
2023-12-02 22:01:48 +0100

    new create s produit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProduitController } from './produit/produit.controller';
+import { ProduitService } from './produit/produit.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ProduitController],
-  providers: [AppService],
+  providers: [AppService, ProduitService],
 })
 export class AppModule {}
